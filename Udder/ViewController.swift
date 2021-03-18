3fdd91611e8b3dd7ae61bc7eac083d5394664ced
Shcro <Schrodinger7Cat@icloud.com>
2021-03-18 13:32:18 +0400

    added something to viewWillDisappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        //
+        print("Good bye, my love, good bye")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
